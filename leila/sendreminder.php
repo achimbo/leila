cdb630c7ef1d738280d6a4bf5b4eae99a1e73953
achim <joachim.boltz@gmx.at>
2015-05-24 13:38:19 +0200

    Added email reminder

---
+++ leila/sendreminder.php
@@ -0,0 +1,40 @@
+<?php
+
+// call daily via cron job
+
+require_once 'variables.php';
+
+session_start();
+if (!isset($_SESSION['usertype']) || $_SESSION['usertype'] != "admin") die ("Bitte <a href='login.php'>anmelden</a>");
+
+$connection = new mysqli ( $db_hostname, $db_username, $db_password, $db_database );
+if ($connection->connect_error)
+	die ( $connection->connect_error );
+
+$query = "SELECT r.duedate, o.name AS objectname, u.firstname, u.lastname, u.email 
+		FROM rented r INNER JOIN objects o ON o.ID = r.objects_ID INNER JOIN users u ON u.ID = r.users_ID 
+		WHERE DATEDIFF(duedate, curdate()) = 3  AND r.givenback IS NULL AND u.email != '' ";
+
+$result = $connection->query($query);
+if (!$result) die ("Database query error" . $connection->error);
+$rows = $result->num_rows;
+
+$subject = "Eine Erinnerung vom Leihladen";
+$headers = "From: info@leihladen.at\r\n";
+$headers .= "Mime-Version: 1.0\r\n";
+$headers .= "Content-type: text/plain; charset=utf-8\r\n";
+
+for ($r = 0; $r < $rows; ++$r) {
+	$result->data_seek($r);
+	$row = $result->fetch_array(MYSQLI_ASSOC);
+	
+	$message = "Hallo {$row['firstname']} \n
+Eine kleine Erinnerung: Du hast dir ein(e) {$row['objectname']} im Leihladen ausgeborgt und solltest es bis {$row['duedate']} zurückgeben. \n
+Liebe Grüße Leihladen Wien\n";
+	
+	if (mail($row['email'], $subject, $message, $headers)) {
+		echo "Email sent to {$row['email']}\n";
+	} else {
+		echo "Email failed";
+	}
+}
